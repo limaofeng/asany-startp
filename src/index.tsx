10a763b604bfdd8e853eeaa920aedf8e2674642d
李茂峰 <limaofeng@msn.com>
2021-09-06 10:29:25 +0800

    fix: 为 HeroCarousel 添加导出

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ export { default } from './sunmao';
 
 export { default as Layout } from './components/_App/Layout';
 export { default as Navbar } from './components/_App/Navbar';
+export { default as HeroCarousel } from './components/Hero/HeroCarousel';
 export { default as AboutUs } from './components/AboutUs';
 export { default as Services } from './components/Services';
 export { default as OurRecentNews } from './components/Blog/OurRecentNews';
-export { default as MainBanner } from './components/DigitalAgencyPortfolio/MainBanner';
 export { default as Cta } from './components/IOT/Cta';
 export { default as FeedbackStyleFive } from './components/Common/FeedbackStyleFive';
 export { default as Partner } from './components/DigitalAgencyPortfolio/Partner';
